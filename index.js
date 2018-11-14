34532d554ce8ddf6601c7864ecd469e452f47f25
EzraMighty <44200559+EzraMighty@users.noreply.github.com>
2018-11-14 01:12:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -53,12 +53,12 @@ bot.on("message", async message => {
   
   if(cmd === `${prefix}ban`) {
 
-    message.author.hasPermission("BAN_MEMBERS")
-    if(!message.author.kick)
-      return message.reply("Sorry, you don't have permissions to use this!")
     let member = message.mentions.members.first();
     if(!member)
       return message.reply("Please mention a valid member of this server");
+    if (!message.member.hasPermission("BAN_MEMBERS"))
+    if(!message.author.kick)
+      return message.reply("Sorry, you don't have permissions to use this!")
     if(!member.bannable) 
       return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
 
