560243bbc0a72548196ace9b151f394ee2358ca5
EzraMighty <44200559+EzraMighty@users.noreply.github.com>
2018-11-20 23:50:00 -0800

    Update afk.js

--- cmds/afk.js
+++ cmds/afk.js
@@ -1,7 +1,7 @@
 module.exports.run = async (client, message, args) => {
 
     let reason = args.join(' ') ? args.join(' ') : 'I am currently afk, I will reply as soon possible.';
-    let afklist = client.afk.get(message.author.id);
+    let afklist = bot.afk.get(message.author.id);
 
     if (!afklist) {
         let construct = {
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
             reason: reason
         };
 
-        client.afk.set(message.author.id, construct);
+        bot.afk.set(message.author.id, construct);
         return message.reply(`you have been set to afk for reason: ${reason}`).then(msg => msg.delete(5000));
     }
 
